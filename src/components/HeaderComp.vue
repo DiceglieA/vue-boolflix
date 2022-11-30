dc17f1a51fab4a221b63d721daae7afc543f611c
DiceglieA <angelodiceglie10@gmail.com>
2022-11-30 16:18:26 +0100

    piccolo modifiche di stile + struttura base header

---
+++ src/components/HeaderComp.vue
@@ -0,0 +1,51 @@
+<template>
+  <header>
+    <div class="title">
+      <a href="!#">BOOLFLIX</a>
+    </div>
+    <div class="selection">
+      <label for="text">
+        <input id="search" type="search" placeholder="digita qui">
+        <button>Invia</button>
+      </label>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'HeaderComp',
+};
+</script>
+
+<style lang="scss" scoped>
+header {
+  height: 10vh;
+  background-color: black;
+  display: flex;
+  justify-content: space-between ;
+  align-items: center;
+  padding: 0 2rem;
+}
+.title {
+  a {
+    color: red;
+    font-size: 30px;
+    font-weight: bolder;
+  }
+}
+#search {
+  border-radius: 20px;
+  padding: 2px 5px;
+  cursor: pointer
+}
+button {
+  margin: 0 .5rem;
+  color: red;
+  background-color: black;
+  border: 1px solid white;
+  border-radius: 20px;
+  padding: 2px 5px;
+  cursor: pointer;
+}
+</style>
---
+++ src/components/MainComp.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MainComp',
+};
+</script>
+
+<style lang="scss" scoped>
+.container {
+  height: 90vh;
+  background-color: rgb(32, 32, 32);
+}
+</style>
